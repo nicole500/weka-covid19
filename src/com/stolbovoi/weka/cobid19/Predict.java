6fb785e317b96f575b37a6c352219f7bd3a2ba68
nicole500 <nicole.stolbovoi@gmail.com>
2022-02-09 17:58:57 +0100

    format

--- src/com/stolbovoi/weka/cobid19/Predict.java
+++ src/com/stolbovoi/weka/cobid19/Predict.java
@@ -3,42 +3,42 @@ package com.stolbovoi.weka.cobid19;
 import java.util.ArrayList;
 
 public class Predict {
-	
+
 	private String name;
 	private long elapsed;
 	private ArrayList<Double> predict;
 	private ArrayList<String> date;
-	
+
 	public String getName() {
-	    return name;
+		return name;
 	}
-	
+
 	public void setName(String o) {
-	    this.name = o;
+		this.name = o;
 	}
-	
+
 	public long getElapsed() {
-	    return elapsed;
+		return elapsed;
 	}
-	
+
 	public void setElapsed(long o) {
-	    this.elapsed = o;
+		this.elapsed = o;
 	}
-	
-	public  ArrayList<Double> getPredict() {
-	    return predict;
+
+	public ArrayList<Double> getPredict() {
+		return predict;
 	}
-	
-	public void setPredict( ArrayList<Double> o) {
-	    this.predict = o;
+
+	public void setPredict(ArrayList<Double> o) {
+		this.predict = o;
 	}
-	
-	public  ArrayList<String> getDate() {
-	    return date;
+
+	public ArrayList<String> getDate() {
+		return date;
 	}
-	
-	public void setDate( ArrayList<String> o) {
-	    this.date = o;
+
+	public void setDate(ArrayList<String> o) {
+		this.date = o;
 	}
-	
+
 }
